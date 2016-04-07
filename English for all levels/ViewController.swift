32d2d7d117ab226579bd47267d5dbc15245a7ded
Hoa C3 <voquanghoa@quang-hoas-macbook-pro.local>
2016-04-07 15:54:51 +0700

    Initial Commit

---
+++ English for all levels/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  English for all levels
+//
+//  Created by Vo Quang Hoa on 4/7/16.
+//  Copyright © 2016 Vo Quang Hoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
