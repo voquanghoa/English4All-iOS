e0fd60d31beb70f848d31ca835fd271f5c916c8f
Vo Quang Hoa <voquanghoa@Vo-Quang-Hoas-MacBook-Pro.local>
2016-04-18 18:10:09 +0700

    Display full screen ads

--- English for all levels/ViewControllers/Lesson/Lesson.swift
+++ English for all levels/ViewControllers/Lesson/Lesson.swift
@@ -9,12 +9,14 @@
 import UIKit
 import GoogleMobileAds
 
-class Lesson: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class Lesson: UIViewController, UITableViewDataSource, UITableViewDelegate, GADInterstitialDelegate {
 
     @IBOutlet weak var bannerView: GADBannerView!
     @IBOutlet weak var questionList: UITableView!
     
     var testContent: TestContent!
+    
+    var interstitial: GADInterstitial!
     
     convenience init(testContent: TestContent){
         self.init()
@@ -31,7 +33,18 @@ class Lesson: UIViewController, UITableViewDataSource, UITableViewDelegate {
         self.questionList.delegate = self
         self.questionList.backgroundColor = UIColor.clearColor()
         GoogleAdsHelper.loadBanner(bannerView, uiViewController: self)
-
+        
+        interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        let request = GADRequest()
+        // Requests test ads on test devices.
+        request.testDevices = ["2077ef9a63d2b398840261c8221a0c9b"]
+        interstitial.loadRequest(request)
+        interstitial.delegate = self
+        
+    }
+    
+    func interstitialDidReceiveAd(ads: GADInterstitial){
+        //interstitial.presentFromRootViewController(self)
     }
     
     override func viewWillAppear(animated: Bool) {
