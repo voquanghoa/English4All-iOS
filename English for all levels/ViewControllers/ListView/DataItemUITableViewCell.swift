37f3945f78de03db3072bdeeb59b63708fd08830
Vo Quang Hoa <voquanghoa@gmail.com>
2016-05-10 23:02:01 +0700

    Change font size

--- English for all levels/ViewControllers/ListView/DataItemUITableViewCell.swift
+++ English for all levels/ViewControllers/ListView/DataItemUITableViewCell.swift
@@ -17,7 +17,7 @@ class DataItemUITableViewCell: UITableViewCell {
     @IBOutlet weak var fileBackground: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        label.font = UIFont.boldSystemFontOfSize(17.0)
+        label.font = UIFont.boldSystemFontOfSize(19.0)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
